5fbe50af100734559803a742daaac0e6a1d96632
himanshuHeliverse <95295861+himanshuHeliverse@users.noreply.github.com>
2021-12-10 11:11:59 +0530

    Update script.js

--- 99 Startup_files/script.js
+++ 99 Startup_files/script.js
@@ -33,6 +33,113 @@ function waitforme(ms) {
 // strings:["First sentence","Second sentence"],
 // });
 
+function changeFeature(n) {
+  console.log(n);
+  switch (n) {
+    case 1:
+      featuresHeading[0].classList.remove("black");
+      featuresHeading[1].classList.add("black");
+      featuresHeading[2].classList.add("black");
+      featuresHeading[3].classList.add("black");
+      featuresHeading[4].classList.add("black");
+      featureImg.src = "/images/features-image/img-1.png";
+      featureTitle.textContent = "A Professional Team at your fingertips";
+      featureDesc.textContent =
+        "Quickly sign up for the services you need. We take care of the quality & the delivery of the work. Sitback relax and get the best talents and services with ease.";
+      document.querySelector(".companies-container").style.display = "flex";
+      document.querySelector(
+        ".companies-container"
+      ).children[1].children[0].src = "/images/company-images/fiverr.png";
+      document.querySelector(
+        ".companies-container"
+      ).children[1].children[1].src = "/images/company-images/upwork.png";
+      document.querySelector(
+        ".companies-container"
+      ).children[2].children[2].src = "/images/company-images/toptal.png";
+      break;
+    case 2:
+      featuresHeading[1].classList.remove("black");
+      featuresHeading[0].classList.add("black");
+      featuresHeading[2].classList.add("black");
+      featuresHeading[4].classList.add("black");
+      featuresHeading[3].classList.add("black");
+      featureImg.src = "/images/features-image/img-2.png";
+      featureTitle.textContent = "Everything from Digital to Design to Tech";
+      featureDesc.textContent =
+        "Say goodbye to the limitations. Whether you’re an early Stage Startup or an Enterprise, we cater to services across categories. Plug & Play subscriptions on the fly.";
+      document.querySelector(".companies-container").style.display = "flex";
+      document.querySelector(
+        ".companies-container"
+      ).children[1].children[0].src = "/images/company-images/fiverr.png";
+      document.querySelector(
+        ".companies-container"
+      ).children[1].children[1].src = "/images/company-images/upwork.png";
+      document.querySelector(
+        ".companies-container"
+      ).children[2].children[2].src = "/images/company-images/toptal.png";
+
+      break;
+    case 3:
+      featuresHeading[2].classList.remove("black");
+      featuresHeading[0].classList.add("black");
+      featuresHeading[1].classList.add("black");
+      featuresHeading[4].classList.add("black");
+      featuresHeading[3].classList.add("black");
+      featureImg.src = "./images/features-image/project.png";
+      featureTitle.textContent = "Collaboration, Team & Project Management";
+      featureDesc.textContent =
+        "Say goodbye to the limitations. Whether you’re an early Stage Startup or an Enterprise, we cater to services across categories. Plug & Play subscriptions on the fly.";
+      document.querySelector(
+        ".companies-container"
+      ).children[1].children[0].src = "/images/company-images/asana.svg";
+      document.querySelector(
+        ".companies-container"
+      ).children[1].children[1].src = "/images/company-images/trello.svg";
+      document.querySelector(
+        ".companies-container"
+      ).children[2].children[2].src = "/images/company-images/jira.svg";
+      document.querySelector(".companies-container").style.display = "flex";
+      break;
+    case 4:
+      featuresHeading[3].classList.remove("black");
+      featuresHeading[1].classList.add("black");
+      featuresHeading[2].classList.add("black");
+      featuresHeading[4].classList.add("black");
+      featuresHeading[0].classList.add("black");
+      featureImg.src = "/images/features-image/img-4.png";
+      featureTitle.textContent = "Collaboration, Team & Project Management";
+      featureDesc.textContent =
+        "We’d love to solve your problems, in fact, we insist. Our support team is personable, friendly, and always available to help. You can always expect a real human to answer all your questions.";
+      document.querySelector(".companies-container").style.display = "none";
+      break;
+    case 5:
+      featuresHeading[3].classList.add("black");
+      featuresHeading[1].classList.add("black");
+      featuresHeading[2].classList.add("black");
+      featuresHeading[4].classList.remove("black");
+      featuresHeading[0].classList.add("black");
+      featureImg.src = "/images/features-image/risk free.png";
+      featureTitle.textContent = "Free-Trials & No-questions asked refunds";
+      featureDesc.textContent =
+        "You’ll fall in love with us. But, If you don’t and you’re not  100% satisfied after a trial, we’ll refund your money back, No questions asked whatsoever. Tryus out now risk-free.";
+      document.querySelector(".companies-container").style.display = "none";
+      break;
+
+
+
+    default:
+      featuresHeading[1].classList.remove("black");
+      featuresHeading[1].classList.add("black");
+      featuresHeading[2].classList.add("black");
+      featuresHeading[3].classList.add("black");
+      featureImg.src = "/images/features-image/img-1.png";
+      document.querySelector(".companies-container").style.display = "flex";
+
+
+
+  }
+}
+
 
 
 // setTimeout(() => {
